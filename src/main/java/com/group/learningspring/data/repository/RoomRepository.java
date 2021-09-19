aa813296f4e55b3f8ba2fc966da6aadb1e90edb8
Alex Wong <alex.wong@orangetesting.com>
2021-09-19 20:02:36 -0300

    Adding exercise files.

---
+++ src/main/java/com/group/learningspring/data/repository/RoomRepository.java
@@ -0,0 +1,10 @@
+package com.group.learningspring.data.repository;
+
+import com.group.learningspring.data.entity.Room;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends CrudRepository<Room, Long> {
+
+}
